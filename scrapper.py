e3821004fcf1f59a630d03c04450fd5512e172c5
Haran <rharan1998@gmail.com>
2018-12-05 19:42:57 +0530

    Made a class method to access tweets according to date more easily

--- scrapper.py
+++ scrapper.py
@@ -5,25 +5,44 @@ from tweepy import API
 import datetime,tweepy
 
 credentials = {}
-credentials['CONSUMER_KEY'] = 'yfnS1Fqzh4EBnqrKXs0C9fnls'
-credentials['CONSUMER_SECRET'] = 'CDX1TDnzQLMuOezhAxLmSerirZLXhAaJuAALxZ4PDtanSn4sT7'
-credentials['ACCESS_TOKEN'] = '2811020677-cygjhFFFyEGhUATHsD1iAbtcP493qZWSO7lYJ8P'
-credentials['ACCESS_SECRET'] = 'BghXsGvcPhkUdlpUsNmjUNZiHE6prPgzlfWVO7D45pM5e'
+
 today = datetime.datetime.now()
 date_limit = today - datetime.timedelta(days=0)
 
-#This is a basic listener that just prints received tweets to stdout.
-class StdOutListener(StreamListener):
 
-    def on_data(self, data):
-        print(data)
-        return True
+class TwitterHandler:
+    def __init__(self):
+        self.credentials = {}
+        self.credentials['CONSUMER_KEY'] = 'yfnS1Fqzh4EBnqrKXs0C9fnls'
+        self.credentials['CONSUMER_SECRET'] = 'CDX1TDnzQLMuOezhAxLmSerirZLXhAaJuAALxZ4PDtanSn4sT7'
+        self.credentials['ACCESS_TOKEN'] = '2811020677-cygjhFFFyEGhUATHsD1iAbtcP493qZWSO7lYJ8P'
+        self.credentials['ACCESS_SECRET'] = 'BghXsGvcPhkUdlpUsNmjUNZiHE6prPgzlfWVO7D45pM5e'
+        self.today = datetime.datetime.now()
+        self.auth = OAuthHandler(self.credentials['CONSUMER_KEY'], self.credentials['CONSUMER_SECRET'])
+        self.auth.set_access_token(self.credentials['ACCESS_TOKEN'], self.credentials['ACCESS_SECRET'])
+        self.api = API(self.auth)
 
-    def on_error(self, status):
-        print (status)
+    def getTweets(self,date,max_tweets,topic):
+        '''
+
+        :param date: python datetime object
+        :param max_tweets: Maximum number of tweets to return
+        :param topic: Topic to search from
+        :return: List of tuples, tuple format (date,tweet)
+        '''
+        from_date = date -datetime.timedelta(days=1)
+        from_date = from_date.strftime("%Y-%m-%d")
+        to_date = date + datetime.timedelta(days=1)
+        to_date = to_date.strftime("%Y-%m-%d")
+        tweets = []
+        for tweet in tweepy.Cursor(api.search, q=topic, since=from_date, until=to_date).items(max_tweets):
+            tweet_date = datetime.datetime.strptime(tweet._json['created_at'], "%a %b %d  %H:%M:%S %z %Y")
+            tweet_text = tweet._json['text']
+            tweets.append((tweet_date,tweet))
+        return tweets
+
 
 if __name__ == '__main__':
-    l = StdOutListener()
     auth = OAuthHandler(credentials['CONSUMER_KEY'], credentials['CONSUMER_SECRET'])
     auth.set_access_token(credentials['ACCESS_TOKEN'], credentials['ACCESS_SECRET'])
     api = API(auth)
