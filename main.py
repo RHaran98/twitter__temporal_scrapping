0173e7409b2f7a483aed900907c9fb374894a082
Haran <rharan1998@gmail.com>
2018-12-07 15:26:11 +0530

    -Added user input -Renamed file for clarity

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ r = Rake()
 t = TwitterHandler()
 
 today = datetime.datetime.now()
-tweets = t.getTimelineTweets(today,10,"#MeToo")
-concat = ''
+query = input("Enter topic to search for : ")
+max_tweets = input("Number of tweets to retrieve per day : ")
+
+tweets = []
+for i in range(1,4):    # Last three days
+    tweets.extend(t.getTimelineTweets(today-datetime.timedelta(days=i),max_tweets,query))
+
 for time,tweet in tweets:
     r.extract_keywords_from_text(tweet)
     events = r.get_ranked_phrases()
